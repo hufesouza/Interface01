c9f6ca2bbdc196f084bab67c36281083ae2be721
Hugo Ferreira <hugof@Hugos-MacBook-Air.local>
2022-04-30 14:12:23 +0100

    first commit

---
+++ InterfaceDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace InterfaceDemo
+    // Interfaces are like contracts, where a class that implements an interface agrees to provide implementation for all
+    // objects defined by that interface. this means an interface will contain the contract terms, methods and properties
+    // but how we implement them is up to the class that implements the inteface.
+    // interfaces cannot contains implementation.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creating objects.
+            Ticket t1 = new Ticket(10);
+            Ticket t2 = new Ticket(10);
+            //Comparing their duration.
+            Console.WriteLine(t1.Equals(t2));
+        }
+    }
+}
---
+++ InterfaceDemo/Ticket.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace InterfaceDemo
+{
+    public class Ticket : IEquatable<Ticket>
+    {
+        // property o store the duration of the ticket in hour.
+        public int DurationInHours { get; set; }
+
+        //simple constructor
+        public Ticket(int durationInHours)
+        {
+            DurationInHours = durationInHours;
+        }
+
+        //method to check if the tickets durations are the same.
+        public bool Equals(Ticket other)
+        {
+            return this.DurationInHours == other.DurationInHours;
+        }
+    }
+}
